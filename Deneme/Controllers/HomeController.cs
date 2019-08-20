d0763c6f6975e4591b68b744afc3b6a99ae337bc
Terzy <gokhantrzgl@gmail.com>
2019-08-20 09:30:33 +0300

    Add project files.

---
+++ Deneme/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Deneme.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new Course();
+            course.Id = 1;
+            course.Name = "Ekranda görünecek yazı";
+
+            
+
+            return View(course);
+        }
+    }
+}
---
+++ Deneme/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deneme.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
